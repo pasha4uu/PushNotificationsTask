cd05a90a59a8dd911b0c3964c1f521ce753063df
pasha4uu <pasha7864u@gmail.com>
2018-12-24 12:14:16 +0530

    Initial Commit

---
+++ PushNotificationsTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushNotificationsTask
+//
+//  Created by PASHA on 24/12/18.
+//  Copyright © 2018 Pasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushNotificationsTask/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PushNotificationsTask
+//
+//  Created by PASHA on 24/12/18.
+//  Copyright © 2018 Pasha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
